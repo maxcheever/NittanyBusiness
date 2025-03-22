cfc3dbbf59963bc6971c1d5b0980b01c76b87953
Sherry Zhang <zsherry168@gmail.com>
2025-03-22 17:21:42 -0400

    Debugging login button

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 host = 'http://127.0.0.1:5000/'
 
 # initialize database when app is run
-# with app.app_context():
-    # initialize_database()
+with app.app_context():
+    initialize_database()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -31,9 +31,10 @@ def check_password(username: str, password: str) -> bool:
 
     # Connect to database
     connection = sql.connect('database.db')
-    info = connection.execute('SELECT * FROM Users WHERE user_id = :username AND password = :password')
-    # if info is valid
-    # return True
+    info = connection.execute('SELECT * FROM Users WHERE user_id = ? AND password = ?', (username, encrypted_password))
+    print(info)
+    if info:
+        return True
 
     return False
 
